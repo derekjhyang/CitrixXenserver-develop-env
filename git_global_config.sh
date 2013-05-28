b1cfd54bf576c04b8c08130e53b797df038bef9b
JuiHao Yang <NCU.PDCLAB.SAMEVED@gmail.com>
2013-05-28 22:58:24 +0800

    add git global config script

---
+++ git_global_config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
