d6b7470ecb369b1c4a3d05e0f4c7f6b4af3c81b5
JuiHao Yang <NCU.PDCLAB.SAMEVED@gmail.com>
2013-05-28 23:19:21 +0800

    add EPEL pkg

--- xenserver-5.6.100.sp2_tools_install.sh
+++ xenserver-5.6.100.sp2_tools_install.sh
@@ -4,3 +4,6 @@ yum --enablerepo=base install -y gcc make automake autoconf vim* \
                                  zlib zlib-devel \
                                  curl curl-devel \
                                  openssl-devel perl cpio expat-devel gettext-devel mlocate
+
+# install EPEL package
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
