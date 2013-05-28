ef50edf5e39a4713e55a61645da0aae7f254fdfe
JuiHao Yang <NCU.PDCLAB.SAMEVED@gmail.com>
2013-05-28 18:16:09 +0800

    fix setuptools install

--- python_tools_install.sh
+++ python_tools_install.sh
@@ -2,16 +2,17 @@
 
 set -x
 
-SETUPTOOLS_SRC=https://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea
-PIP_SRC=https://pypi.python.org/packages/source/p/pip/pip-1.3.1.tar.gz#md5=cbb27a191cebc58997c4da8513863153
-SETUPTOOLS_EGG=$(mktemp)
-PIP_TARBALL=$(mktemp)
-WORK_DIR=$(mktemp -d)
 # setuptools install
-wget --no-check-certificate -qO ${SETUPTOOLS_EGG} ${SETUPTOOLS_SRC}
+SETUPTOOLS_SRC=https://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea
+wget --no-check-certificate ${SETUPTOOLS_SRC}
+SETUPTOOLS_EGG=$(find -maxdepth 1 -name 'setuptools*.egg' -type f)
 sh ${SETUPTOOLS_EGG}
+rm ${SETUPTOOLS_EGG}
 
 # pip install
+PIP_SRC=https://pypi.python.org/packages/source/p/pip/pip-1.3.1.tar.gz#md5=cbb27a191cebc58997c4da8513863153
+PIP_TARBALL=$(mktemp)
+WORK_DIR=$(mktemp -d)
 wget --no-check-certificate -qO ${PIP_TARBALL} ${PIP_SRC}
 tar zxvf ${PIP_TARBALL} -C ${WORK_DIR}
 cd ${WORK_DIR}
