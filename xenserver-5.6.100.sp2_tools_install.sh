2b6220c8efa7ae418439ea593585464517a1efb9
JuiHao Yang <NCU.PDCLAB.SAMEVED@gmail.com>
2013-05-28 19:59:12 +0800

    update xenserver tools install

--- xenserver-5.6.100.sp2_tools_install.sh
+++ xenserver-5.6.100.sp2_tools_install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-yum --enablerepo=base install -y gcc make automake autoconf vim*
-yum --enablerepo=base install -y zlib zlib-devel
-yum --enablerepo=base install -y curl curl-devel
-yum --enablerepo=base install -y openssl-devel perl cpio expat-devel gettext-devel
+yum --enablerepo=base install -y gcc make automake autoconf vim* \
+                                 zlib zlib-devel \
+                                 curl curl-devel \
+                                 openssl-devel perl cpio expat-devel gettext-devel mlocate
